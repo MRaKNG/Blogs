9c4511e48e032f5bfd11afcec038954b38a2137e
MRaKNG <46597799+MRaKNG@users.noreply.github.com>
2022-06-30 09:57:14 +0400

    Create index.php
    start

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+  <meta charset="UTF-8">
+  <title>Новый сайт успешно создан и готов к работе</title>
+  <!-- b0a8e2d8ccb04b24683d347076e80d29e451a385:d3aa2e6571e673001cb012eda23bd97d02234f0b -->
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+ <script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
+  
+</head>
+<body>
+Hello world!!
+</body>
+</html>
